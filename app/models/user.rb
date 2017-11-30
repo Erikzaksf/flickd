a3bed38626558d6a5ff89df9fb10e78d77aa1da7
Dan Nollette and Erik Zakrzewski <dan+erik@>
2017-11-30 11:22:49 -0800

    add signup reference as root route

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :images, through: :user_img_relations
   has_many :user_img_relations
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
